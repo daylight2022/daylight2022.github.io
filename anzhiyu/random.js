6044083894c30188f36224d03b8b26626aa7091b
daylight2022 <1514853542@qq.com>
2024-09-29 15:59:08 +0800

    Site updated: 2024-09-29 15:59:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/19/hello-world/","2024/09/24/Next-Auth + Prisma，Github OAuth 登录功能详解/"];function toRandomPost(){
+var posts=["2024/09/24/Next-Auth + Prisma，Github OAuth 登录功能详解/","2024/09/27/会员积分系统设计开发/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
